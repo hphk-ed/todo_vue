ec86ddc8d8dbaf6b19911e1cdc8c6ef234d29757
Edwin <hyena.edwin@gmail.com>
2020-06-07 05:12:06 +0900

    modified title

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Todo Todo Ddo</h1>
+    <h1>Todo Todo Ddu</h1>
     <TodoInput @new-todo="createTodo"/>
     <TodoList :todoList="todos" @update="updateTodo" @delete="deleteTodo"/>
   </div>
